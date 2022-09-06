c237c26261d099475505dfd21df003df38f40052
emsweetland <emmaksweetland@gmail.com>
2022-09-06 14:08:37 -0500

    wrote SQL for create and join tables

--- database.sql
+++ database.sql
@@ -1,10 +1,41 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
+--create tables--
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" SERIAL PRIMARY KEY,
+	"username" VARCHAR(80) UNIQUE NOT NULL,
+	"password" VARCHAR(1000) NOT NULL
 );
+
+
+CREATE TABLE "mood" (
+	"id" SERIAL PRIMARY KEY,
+	"moodname" VARCHAR(50) NOT NULL,
+	"imageurl" VARCHAR(2000) NOT NULL
+);
+
+
+CREATE TABLE "reflection" (
+	"id" SERIAL PRIMARY KEY,
+	"when" TIMESTAMP,
+	"user_id" INT REFERENCES "user",
+	"mood_id" INT REFERENCES "mood"
+);
+
+CREATE TABLE "prompt" (
+	"id" SERIAL PRIMARY KEY,
+	"text" VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE "response" (
+	"id" SERIAL PRIMARY KEY,
+	"reflection_id" INT REFERENCES "reflection",
+	"prompt_id" INT REFERENCES "prompt",
+	"response" BOOLEAN
+);
+
+--join tables--
+SELECT "user".username, "reflection".id AS "reflection_id", "mood".moodname, "prompt".text, "response".response FROM "user"
+JOIN "reflection" ON "user".id = "reflection".user_id
+JOIN "mood" ON "mood".id = "reflection".mood_id
+JOIN "response" ON "response".reflection_id = "reflection".id
+JOIN "prompt" ON "response".prompt_id = "prompt".id
+GROUP BY "user".username, "reflection".id, "mood".moodname, "prompt".text, "response".response
